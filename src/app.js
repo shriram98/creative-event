c5153c6f60b8b945a3f12c4d91a92bfa3f121853
shriram <shriramvenugopal123@gmail.com>
2018-11-02 11:48:22 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -7,11 +7,25 @@ export default class App extends React.Component {
             super(props)
             this.state = {
                 name: []
+
             }
         }
         render() {
             return(
-                <div>
+                <div class="email-box">
+                    <div class="first-part">
+                        <ul class="taskbar">
+                            <li id="compose" class="taskbar-item">Compose</li>
+                            <li id="inbox" class="taskbar-item">inbox</li>
+                            <li id="starred" class="taskbar-item">starred</li>
+                            <li id="drafts" class="taskbar-item">Drafts</li>
+                            <li id="archive" class="taskbar-item">Archive</li>
+                            <li id="sentItems" class="taskbar-item">Sent Items</li>
+                            <li id="spam" class="taskbar-item">Spam</li>
+                        </ul>
+                    </div>
+                    <div class="second-part">hi</div>
+                    <div class="third-part">hi</div>
                 </div>
             )
         }
