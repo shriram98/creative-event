3cb8d1bbfbeafe35c4a1e8e05015e83e5376b8ca
Pallavi V G <pallavivg98@gmail.com>
2018-11-02 12:14:42 +0530

    ded

--- src/app.js
+++ src/app.js
@@ -6,7 +6,14 @@ export default class App extends React.Component {
         constructor(props)  {
             super(props)
             this.state = {
-                name: []
+                email: [
+                    {sender:"John Doe",
+                subject:"Regarding Meeting"},
+                {sender:"John Doe",
+                subject:"Regarding Meeting"},
+                {sender:"John Doe",
+                subject:"Regarding Meeting"},
+                ]
 
             }
         }
@@ -15,16 +22,59 @@ export default class App extends React.Component {
                 <div class="email-box">
                     <div class="first-part">
                         <ul class="taskbar">
-                            <li id="compose" class="taskbar-item">Compose</li>
-                            <li id="inbox" class="taskbar-item">inbox</li>
-                            <li id="starred" class="taskbar-item">starred</li>
-                            <li id="drafts" class="taskbar-item">Drafts</li>
-                            <li id="archive" class="taskbar-item">Archive</li>
-                            <li id="sentItems" class="taskbar-item">Sent Items</li>
-                            <li id="spam" class="taskbar-item">Spam</li>
+                            <li id="compose" class="taskbar-item"><div className="label">Compose</div></li>
+                            <li id="inbox" class="taskbar-item"><div className="label">Inbox</div></li>
+                            <li id="starred" class="taskbar-item"><div className="label">Starred</div></li>
+                            <li id="drafts" class="taskbar-item"><div className="label">Drafts</div></li>
+                            <li id="archive" class="taskbar-item"><div className="label">Archive</div></li>
+                            <li id="sentItems" class="taskbar-item"><div className="label">Sent Items</div></li>
+                            <li id="spam" class="taskbar-item"><div className="label">Spam</div></li>
                         </ul>
                     </div>
-                    <div class="second-part">hi</div>
+                   
+            <div class="second-part">
+            <ul>
+                <li>
+                    <div class="mini-grid">
+                    <div class="circle">
+                    </div>
+                    <div>
+                    <h2 class="mail-sender">
+                        John Doe
+                    </h2>
+                    <p class="mail-subject">Hello there spam mail...</p>
+                    </div>
+                    <div class="forward-icon">
+                    </div>
+                    </div>
+                </li>
+                <li>
+                        <div class="mini-grid">
+                        <div class="circle">
+                        </div>
+                        <div>
+                        <h2 class="mail-sender">
+                            John Doe
+                        </h2>
+                        <p class="mail-subject">Hello there spam mail...</p>
+                        </div>
+                        </div>
+                    </li>
+                    <li>
+                            <div class="mini-grid">
+                            <div class="circle">
+                            </div>
+                            <div>
+                            <h2 class="mail-sender">
+                                John Doe
+                            </h2>
+                            <p class="mail-subject">Hello there spam mail...</p>
+                            </div>
+                            </div>
+                        </li>
+            
+            </ul>
+            </div>
                     <div class="third-part">hi</div>
                 </div>
             )
